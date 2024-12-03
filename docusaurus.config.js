3d486033d8beaaebf6034e35167624a35a294d1f
Cristiano Santos <85245@ualg.pt>
2024-12-03 10:43:12 +0000

    Mudanças NavBar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Test Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'CAFH',
+  tagline: 'ADC É BARIL',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -65,7 +65,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Biblioteca CAFH',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -75,7 +75,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Classes',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
