d3cd8b7b4a16975dfcfda42bb371af424f4325ea
Cristiano Santos <85245@ualg.pt>
2024-12-10 16:20:05 +0000

    alteração da descrição nas descrições das imagens na home page

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,32 +10,32 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'Fácil de Usar',
     Svg: require('@site/static/img/123.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        A aplicação de Gestão de Biblioteca foi projetada desde o início para ser facilmente instalada 
+        e utilizada, permitindo que comece rapidamente a gerir a sua biblioteca de forma eficiente.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Gestão Simples e Eficaz',
     Svg: require('@site/static/img/1234.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        A nossa aplicação foi desenvolvida para oferecer uma experiência intuitiva, garantindo que as tarefas
+        de gestão de livros, leitores e empréstimos sejam realizadas de forma prática e sem complicações.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Funcionalidades Abrangentes',
     Svg: require('@site/static/img/file.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Com um conjunto completo de ferramentas, a aplicação permite realizar operações como registo de 
+        livros, monitorização de empréstimos e notificações de prazos, assegurando um controlo total sobre a sua biblioteca.
       </>
     ),
   },
