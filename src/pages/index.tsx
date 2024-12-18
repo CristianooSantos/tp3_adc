d884493acd483ec6d7d9cc140570a85b6b9da89c
Cristiano Santos <85245@ualg.pt>
2024-12-18 16:55:27 +0000

    estilo atualizado no index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
-import styles from './index.module.css';
+import styles from './styles.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/aboutUs">
+          <Link className="button button--secondary button--lg" to="/docs/aboutUs">
             About Us
           </Link>
         </div>
@@ -27,11 +25,28 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
+
+  // Scroll event for fade-in effect
+  useEffect(() => {
+    const handleScroll = () => {
+      const featureCards = document.querySelectorAll('.featureCard');
+      featureCards.forEach((card) => {
+        const rect = card.getBoundingClientRect();
+        if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+          card.classList.add('visible');
+        }
+      });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll(); // Trigger the scroll effect on initial load
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+    <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
